0e61ce5e0975631f8c4bf4e4968705aa45b9fe46
NPEX42 <NPEX42@gmail.com>
2023-08-19 16:46:09 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir "../lib" 
+    objdir "./obj" 
+
+    files {
+        "imgui.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3_loader.h",
+        "imconfig.h",
+        "imgui.h",
+        "imgui_internal.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+    }
+
+    includedirs {".", "../glfw/include"}
